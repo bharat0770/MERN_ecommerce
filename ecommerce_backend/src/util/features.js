8c7ba17a9a5df49fd2cfee877ec3187341ab5efe
bharat0770 <bharats0770@gmail.com>
2024-09-02 21:37:46 +0530

    renaming app to index

--- ecommerce_backend/src/util/features.js
+++ ecommerce_backend/src/util/features.js
@@ -9,6 +9,7 @@ const connectDB = () => {
     // .then((c) => { console.log(`DB connectred to ${c.connection.host}`)})
     // .catch((e) => {console.log(e)});
     // return mongoose.connect("mongodb://localhost:27017/Ecommerce_24");
+    
     return mongoose.connect(process.env.MONGO_URL);
 };
 
